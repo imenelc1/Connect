3262d9bce54ca68cf214b14bc6914ea9118eb7ae
meriem <meriemhamouchepro@gmail.com>
2025-11-06 01:01:54 +0100

    BDD

---
+++ frontend/src/components/common/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LogoLight from "../../assets/LogoLight.svg";
+
+
+ export default function Logo() {
+    return (
+        <img 
+        src={""+LogoLight+""}
+        alt= "logo" 
+        />
+    );
+}
