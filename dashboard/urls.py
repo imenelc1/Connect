546511f986407444f8454884705b3af6b4f7bb92
meriem <meriemhamouchepro@gmail.com>
2025-11-11 23:06:03 +0100

    start Badges

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from .views import ProgressionCoursViewSet
+from .views import BadgeViewSet
 
 router = DefaultRouter()
 router.register(r'progressions', ProgressionCoursViewSet)
-
+router.register(r'badges', BadgeViewSet)
 urlpatterns = router.urls
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from .models import ProgressionCours
 from .serializers import ProgressionCoursSerializer
+from .models import Badge
+from .serializers import BadgeSerializer
 
 class ProgressionCoursViewSet(viewsets.ModelViewSet):
     queryset = ProgressionCours.objects.all()
     serializer_class = ProgressionCoursSerializer
+class BadgeViewSet(viewsets.ModelViewSet):
+    queryset = Badge.objects.all()
+    serializer_class = BadgeSerializer
