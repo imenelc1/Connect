044bfda374eee65cbcfa0fed57fcd507dd0ba4d1
imene <imene6219@gmail.com>
2025-11-18 21:30:23 +0100

    ajouter point pour users backend

--- dashboard/models.py
+++ dashboard/models.py
@@ -23,3 +23,27 @@ class Analyse(models.Model):
     exercice = models.ForeignKey(Exercice, on_delete=models.CASCADE)
     explication_ia = models.TextField()
     date_analyse = models.DateField()
+
+class Badge(models.Model):
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    icone = models.ImageField(upload_to='badges/', null=True, blank=True)
+    condition = models.CharField(max_length=255)
+    categorie = models.CharField(max_length=50, default="Général")
+    numpoints = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.nom
+
+
+class GagnerBadge(models.Model):
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    badge = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    date_obtention = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('utilisateur', 'badge')  # un badge ne peut être gagné qu'une fois
+
+    def __str__(self):
+     return f"{self.utilisateur} earned {self.badge.nom}"
