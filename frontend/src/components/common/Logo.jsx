cffd926d0c7349530e038aeb44721e51cf721d23
imene <imene6219@gmail.com>
2025-11-19 21:27:30 +0100

    Synchronisation complète du projet

--- frontend/src/components/common/Logo.jsx
+++ frontend/src/components/common/Logo.jsx
@@ -1,7 +1,13 @@
+import React from "react";
+import logo from "../../assets/LogoLight.svg";
+
 export default function Logo() {
   return (
-    <h1 className="text-2xl font-extrabold text-blue-500">
-      CONNECT<span className="text-gray-500">;</span>
-    </h1>
+    <div className="flex items-center space-x-2">
+      <img src={logo} alt="logo" className="w-32 h-32" />
+      {/* <span className="text-xl font-bold text-[var(--color-primary)]"></span> */}
+    </div>
   );
 }
+
+ 
