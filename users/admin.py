0bf129f4bc6bba099337bc1fd78cd9e67631ddee
meriem <meriemhamouchepro@gmail.com>
2025-11-23 02:30:55 +0100

    dark mode login et signup

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Utilisateur, Etudiant, Enseignant, Administrateur
 
-admin.site.register(Utilisateur)
-admin.site.register(Etudiant)
-admin.site.register(Enseignant)
-admin.site.register(Administrateur)
+# Register your models here.
