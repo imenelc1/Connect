5b9f0680692e8c50c9a4b44fe42fc39f4cc1e54a
imene <imene6219@gmail.com>
2025-12-21 22:39:03 +0100

    Corriger la casse du dossier Exercices → exercices

--- Exercices/app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class ExercicesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'exercices'
-
-    def ready(self):
-        import exercices.signals
