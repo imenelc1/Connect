19d85861bfc7fab197fbcfe7aee22d83afcbaba0
imene <imene6219@gmail.com>
2026-01-06 08:23:59 +0100

    jkjk

--- frontend/src/components/common/Input.jsx
+++ frontend/src/components/common/Input.jsx
@@ -22,7 +22,7 @@ export default function Input({
         className={`flex items-center rounded-xl px-4 py-3 border
           bg-[rgb(var(--color-input-bg))]
           text-[rgb(var(--color-input-text))]
-          ${error ? "border-red-500" : "border-[rgb(var(--color-input-border))]"}
+          ${error ? "border-red" : "border-[rgb(var(--color-input-border))]"}
           focus-within:ring-2 focus-within:ring-[rgb(var(--color-primary))]
           transition
           ${className}
@@ -50,7 +50,7 @@ export default function Input({
       </div>
 
       {error && (
-        <p className="text-red-500 text-xs mt-1">
+        <p className="text-red text-xs mt-1">
           {error}
         </p>
       )}
