c5e71f5242ac2c8c6834518ab83ed186aedcf2cc
imenelc1 <150154190+imenelc1@users.noreply.github.com>
2026-02-19 13:19:59 +0100

    Update urls.py

--- connect_backend/urls.py
+++ connect_backend/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # API apps
     path('api/', include('forum.urls')), 
     path('api/users/', include('users.urls')),
     path('api/courses/', include('courses.urls')),
@@ -13,16 +18,16 @@ urlpatterns = [
     path('api/dashboard/', include('dashboard.urls')),
     path('api/', include('feedback.urls')),
     path('api/badges/', include('badges.urls')),
-
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/spaces/', include('spaces.urls')),
 
-    
-   
+    # JWT Token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # Redirection racine vers /api/
+    path('', RedirectView.as_view(url='/api/', permanent=True)),
 ]
 
+# Media en mode debug
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
